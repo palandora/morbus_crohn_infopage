a0133e4cabb8c06eaf7690f8e3303699568a7c12
palandora <bernd@dotsandlines.io>
2021-08-01 13:31:46 +0200

    added both vertical & horizontal card flipping to css

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 
 function flipCard(){
     //get all cards
-    let flipped = false;
+    let flippedD = false;
+    let flippedC = false; 
     const card_diarrhea = document.querySelector('.diarrhea');
+    const card_cases = document.querySelector('.cases');
+    console.log(card_cases.childNodes[1])
     const innerCard = card_diarrhea.childNodes[1];
 
     card_diarrhea.addEventListener('click',()=>{
-        if(!flipped){
+        if(!flippedD){
             console.log("-")
             innerCard.style.transform = "perspective(800px) rotateY(180deg)"
-            flipped = true
+            flippedD = true
         }else{
             innerCard.style.transform = "perspective(800px) rotateY(0deg)"
-            flipped = false
+            flippedD = false
         }
         
     });
+    card_cases.addEventListener('click', ()=>{
+        
+        card_cases.childNodes[1].classList.contains('flipX') ? card_cases.childNodes[1].classList.remove('flipX') :card_cases.childNodes[1].classList.add('flipX');  
+            
+
+    })
 }
 
 
