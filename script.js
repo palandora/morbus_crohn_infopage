2058e0a799af262aea99623f4129a81a9b2aa354
palandora <bernd@dotsandlines.io>
2021-08-01 17:30:34 +0200

    finsished js for flip effect

--- script.js
+++ script.js
@@ -1,29 +1,18 @@
 
 function flipCard(){
     //get all cards
-    let flippedD = false;
-    let flippedC = false; 
-    const card_diarrhea = document.querySelector('.diarrhea');
-    const card_cases = document.querySelector('.cases');
-    console.log(card_cases.childNodes[1])
-    const innerCard = card_diarrhea.childNodes[1];
+    const cards = document.querySelectorAll('.container_faces')
 
-    card_diarrhea.addEventListener('click',()=>{
-        if(!flippedD){
-            console.log("-")
-            innerCard.style.transform = "perspective(800px) rotateY(180deg)"
-            flippedD = true
-        }else{
-            innerCard.style.transform = "perspective(800px) rotateY(0deg)"
-            flippedD = false
-        }
-        
-    });
-    card_cases.addEventListener('click', ()=>{
-        
-        card_cases.childNodes[1].classList.contains('flipX') ? card_cases.childNodes[1].classList.remove('flipX') :card_cases.childNodes[1].classList.add('flipX');  
-            
-
+    cards.forEach(card =>{
+        card.addEventListener('click',()=>{
+            const innerCard = card.childNodes[1]
+            const classes = innerCard.classList
+            if(card.classList.contains('extended')){
+                classes.contains('flipX') ? classes.remove('flipX') : classes.add('flipX')
+            }else{
+                classes.contains('flipY') ? classes.remove('flipY') : classes.add('flipY')
+            }
+        })
     })
 }
 
