5ae61552ade4c8550046f3fa450bf4ff5de2885f
palandora <bernd@dotsandlines.io>
2021-08-03 19:43:26 +0200

    added overlay function to js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function setOverlay(show,htmlEl){
+    if(show){
+        htmlEl.style.zIndex = "2"
+        document.body.classList.add('hide')
+    }else{
+        htmlEl.style.zIndex = "0"
+        document.body.classList.remove('hide')
+    }
+}
+
+
 
 function flipCard(){
     document.addEventListener('click', e =>{
@@ -15,16 +26,22 @@ function flipCard(){
                 if(classListFaces.contains('flipX')){
                     frontFace.style.display = "block"
                     classListFaces.remove('flipX')
+                    setOverlay(false, faces)
                 }else{
                     setTimeout(()=>{frontFace.style.display = "none"}, 500) 
                     classListFaces.add('flipX')
+                    setOverlay(true, faces)
                 }
             }else{
-                classListFaces.contains('flipY') ? classListFaces.remove('flipY') : classListFaces.add('flipY')
+                if(classListFaces.contains('flipY')){
+                    classListFaces.remove('flipY')
+                    setOverlay(false, faces)
+                }else{
+                    classListFaces.add('flipY')
+                    setOverlay(true, faces)
+                }
             }
-
         }
-        
     })
 }
 
